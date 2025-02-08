d78832f5c7858c2989239cb130dffa3ce30df477
klaud <klaudiocarvalho5@gmail.com>
2025-02-08 15:23:45 -0300

    Aula Eliot Animais

--- src/weslley/br/Main.java
+++ src/weslley/br/Main.java
@@ -1,17 +1,37 @@
 package weslley.br;
 
-import weslley.br.models.Pessoa;
+import weslley.br.models.Animal;
+import weslley.br.models.Cachorro;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Pessoa klaudio = new Pessoa();
 
-		klaudio.setNome("Klaudio saddsdsd");
-		klaudio.setIdade(20);
-		klaudio.setSobreNome("Chan");
+		List<Animal> animaisList = new ArrayList<>();
 
-		System.out.println(klaudio.getNome());
+		Animal toto = new Animal("Toto", "Viralata", "Caramelo", 3);
+		animaisList.add(toto);
+
+		Animal tubarao = new Animal("Tubarão", "Viralata", "Branco com manchas pretas", 6);
+		animaisList.add(tubarao);
+
+		Animal chico = new Animal("Chico", "Viralata", "Preto", 10);
+		animaisList.add(chico);
+
+		Animal loro = new Animal("Loro", 20);
+		animaisList.add(loro);
+
+		Cachorro cachorro = new Cachorro("Joao", "Poodle", "Branco", 10);
+
+		cachorro.getNome();
+
+		for (Animal animal : animaisList) {
+			System.out.println(animal.toString());
+
+		}
 
 	}
 
