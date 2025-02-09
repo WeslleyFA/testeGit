124eaa97a5b7a4fd28f716daa3ace38b6263bb1e
Klaudio Eduardo <klaudiocarvalho5@gmail.com>
2025-02-09 20:46:58 -0300

    Organizei o codigo

--- src/weslley/br/Main.java
+++ src/weslley/br/Main.java
@@ -1,56 +1,13 @@
 package weslley.br;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import weslley.br.models.Animal;
-
 public class Main {
 
 	public static void main(String[] args) {
+		
+		ResolucaoKlaudioAnimais.resultadoKlaudioAnimais();
+		
 
-		Animal animal = new Animal();
-		Scanner teclado = new Scanner(System.in);
-		String resposta;
-		Boolean continuarCadastrando = true;
-		List<Animal> animais = new ArrayList<>();
-
-		while (continuarCadastrando) {
-
-			System.out.println("Escreva o nome do seu animal:");
-			animal.setNome(teclado.next());
-
-			System.out.println("Escreva a raça do seu animal:");
-			animal.setRaça(teclado.next());
-
-			System.out.println("Escreva a cor do seu animal:");
-			animal.setCor(teclado.next());
-
-			System.out.println("Escreva a idade do seu animal:");
-			animal.setIdade(teclado.nextInt());
-
-			animais.add(animal);
-
-			System.out.println("Você deseja continuar cadastrando? (s/n)");
-			resposta = teclado.next();
-
-			if (resposta.equalsIgnoreCase("s") || resposta.equalsIgnoreCase("Sim")) {
-				continuarCadastrando = true;
-
-			} else if (resposta.equalsIgnoreCase("n") || resposta.equalsIgnoreCase("Não")) {
-				continuarCadastrando = false;
-
-			} else {
-				System.out.println("Sua resposta está incorreta.\nProjeto encerrado, aqui está o cadastro.");
-				continuarCadastrando = false;
-			}
-
-		}
-
-		for (Animal animalSelecionado : animais) {
-			System.out.println(animalSelecionado);
-		}
+		
 	}
 
 }
